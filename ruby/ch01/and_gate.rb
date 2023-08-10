f517ebcf5be9b97ede4b2c5b793a583f461ec36c
Andersen Fan <as181920@gmail.com>
2023-08-10 20:52:14 +0800

    Chore: implement ch01 and_gate

--- ruby/ch01/and_gate.rb
+++ ruby/ch01/and_gate.rb
@@ -1,7 +1,17 @@
+require "torch-rb"
+
 module Perceptron
   module_function
 
   def and_gate(x1, x2)
-    false
+    w1, w2, theta = 0.5, 0.5, 0.7 # rubocop:disable Style/ParallelAssignment
+    ((x1 * w1) + (x2 * w2)).then { |sum| sum <= theta ? 0 : 1 }
+  end
+
+  def and_gate_with_bias(x1, x2)
+    x = Torch.tensor([x1, x2])
+    w = Torch.tensor([0.5, 0.5])
+    b = -0.7
+    ((x * w).sum + b) <= 0 ? 0 : 1
   end
 end
--- ruby/ch01/and_gate_test.rb
+++ ruby/ch01/and_gate_test.rb
@@ -3,6 +3,16 @@ require_relative "and_gate"
 
 describe Perceptron do
   it "should calc and gate" do
-    assert_equal 0, Perceptron.and_gate(0.1, 0.2)
+    assert_equal 0, Perceptron.and_gate(0, 0)
+    assert_equal 0, Perceptron.and_gate(0, 1)
+    assert_equal 0, Perceptron.and_gate(1, 0)
+    assert_equal 1, Perceptron.and_gate(1, 1)
+  end
+
+  it "should calc and gate with bias" do
+    assert_equal 0, Perceptron.and_gate_with_bias(0, 0)
+    assert_equal 0, Perceptron.and_gate_with_bias(0, 1)
+    assert_equal 0, Perceptron.and_gate_with_bias(1, 0)
+    assert_equal 1, Perceptron.and_gate_with_bias(1, 1)
   end
 end
