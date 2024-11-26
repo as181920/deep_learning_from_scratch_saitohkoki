58d78bd48e9a311e373565f697532a1b113b8025
Andersen Fan <as181920@gmail.com>
2024-11-26 13:53:23 +0800

    Chore: add ch05 affine_layer without test coverage

---
+++ ruby/ch05/affine_layer.rb
@@ -0,0 +1,24 @@
+require "torch-rb"
+require_relative "../common/global"
+
+class AffineLayer
+  attr_reader :w, :b, :x, :dw, :db
+
+  def initialize(w:, b:)
+    @w = w
+    @b = b
+  end
+
+  def forward(x)
+    @x = x
+    Torch::Linalg.multi_dot([x, w]) + b
+  end
+
+  def backward(dout)
+    dx = Torch::Linalg.multi_dot(dout, w.transpose(0, 1))
+    @dw = Torch::Linalg.multi_dot(x.transpose(0, 1), dout)
+    @db = Torch.sum(dout, 0)
+
+    dx
+  end
+end
