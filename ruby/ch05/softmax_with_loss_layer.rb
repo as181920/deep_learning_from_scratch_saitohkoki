c88c5c2c466d08b06d2e2773473f60a5a16cdc3c
Andersen Fan <as181920@gmail.com>
2024-11-26 14:37:24 +0800

    Chore: add ch05 softmax_with_loss layer without test coverage

---
+++ ruby/ch05/softmax_with_loss_layer.rb
@@ -0,0 +1,20 @@
+require "torch-rb"
+require_relative "../common/global"
+require_relative "../ch04/loss"
+
+class SoftmaxWithLossLayer
+  attr_reader :loss, :y, :t
+
+  def forward(x, t)
+    @t = t
+    @y = Torch.softmax(x, dim: 0)
+    @loss = Loss.cross_entropy_error(y, t)
+
+    loss
+  end
+
+  def backward(_dout = 1)
+    batch_size = t.shape[0]
+    (y - t) / batch_size
+  end
+end
